879b025a479c8a9fe273d077ab961f79a25815a2
Shruti <shrutigawande1910@gmail.com>
2023-08-25 12:50:59 +0530

    docs : changed origin link

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
  app.use(cors({
     credentials:true,
-    origin:["https://workwisee.netlify.app/"]
+    origin:["https://work-wise-fe.vercel.app"]
  }));
 
 
